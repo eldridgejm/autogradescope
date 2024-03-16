f1d842c8ec6576248cedbce9aa4c15fff15b709d
Justin Eldridge <eldridgejm@gmail.com>
2024-03-16 09:52:09 -0700

    Started docs.

---
+++ doc/source/index.rst
@@ -0,0 +1,24 @@
+.. autogradescope documentation master file, created by
+   sphinx-quickstart on Sat Mar 16 09:49:20 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+autogradescope
+==============
+
+`autogradescope` is a Python package that provides a simple interface to the
+Gradescope autograding API. It enables writing autograders using `pytest` unit
+tests and provides other useful utilities for autograding.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- flake.nix
+++ flake.nix
@@ -1,30 +1,34 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/21.11;
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-    in
-      rec {
-
-        devShell = forAllSystems (system:
-          let pkgs = import nixpkgs { system = system; };
-          in
-            pkgs.mkShell {
-            buildInputs = [
-              (
-                pkgs.python3.withPackages (ps: 
-                  [
-                    ps.pytest
-                    ps.black
-                    ps.rich
-                  ]
-                )
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+  in rec {
+    devShell = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {system = system;};
+      in
+        pkgs.mkShell {
+          buildInputs = [
+            (
+              pkgs.python3.withPackages (
+                ps: [
+                  ps.pytest
+                  ps.black
+                  ps.rich
+                ]
               )
-            ];
-          }
-        );
-      };
-
+            )
+          ];
+          nativeBuildInputs = with pkgs.python3Packages; [
+            sphinx
+            sphinx_rtd_theme
+          ];
+        }
+    );
+  };
 }
